2ce6b6bf1b58b8d12e2779b7c13740a86bb28af5
Beto Esquivel <ppbeto94@gmail.com>
2016-03-03 13:12:17 +0000

    Fixes gitignored files and finds links

--- testdataextractor/extractor.py
+++ testdataextractor/extractor.py
@@ -35,12 +35,60 @@ class Extractor:
 
         parsed_comments = self.parse_comments(soup.findAll('comment'))
 
+        parsed_links = self.parse_links(soup.findAll(lambda tag:Extractor.is_valid_link(tag)))
+
         article = {
             'sentences': parsed_sentences,
-            'comments': parsed_comments
+            'comments': parsed_comments,
+            'links': parsed_links
         }
 
         return article
+
+    @staticmethod
+    def is_valid_link(tag):
+        if tag.name != 'link':
+            return False
+        link = tag
+
+        l_conf = link['link_confidence']
+        l_val = link['validation']
+        arg = link.find_next_sibling('argument')
+        sent = link.find_next_sibling('sentiment')
+        a_val = arg['validation']
+        s_val = sent['validation']
+        a_conf = arg['val_confidence']
+        s_conf = sent['val_confidence']
+        args = [l_val, a_val, s_val, l_conf, a_conf, s_conf]
+
+        return all(el == '1' or el == 'yes' for el in args)
+
+    def parse_links(self, links):
+        parsed_links = {}
+        for link_html in links:
+            arg_html = link_html.find_next_sibling('argument')
+            sent_html = link_html.find_next_sibling('sentiment')
+            link = {}
+            link['art_sentence'] = link_html['art_sentence']
+            link['com_sentence'] = link_html['com_sentence']
+            link['confidence'] = link_html['link_confidence']
+            link['validation'] = link_html['validation']
+
+            arg = {}
+            arg['label'] = arg_html['label']
+            arg['confidence'] = arg_html['val_confidence']
+            arg['validation'] = arg_html['validation']
+
+            sent = {}
+            sent['label'] = sent_html['label']
+            sent['confidence'] = sent_html['val_confidence']
+            sent['validation'] = sent_html['validation']
+
+            link['argument'] = arg
+            link['sentiment'] = sent
+            parsed_links[link_html['id']] = link
+        return parsed_links
+
 
     def extract(self, dtype='dict', verbose=False):
         f = open(self.file_name, 'r')
@@ -50,7 +98,8 @@ class Extractor:
 
         if verbose:
             print len(article['comments'].values()), " comments parsed."
-            print len(article['sentences'].values()), " sentences parsed."
+            print len(article['sentences'].values())," sentences parsed."
+            print len(article['links'].values()),    " links parsed."
 
         if dtype is 'json':
             json_article = json.dumps(article, indent=4)
